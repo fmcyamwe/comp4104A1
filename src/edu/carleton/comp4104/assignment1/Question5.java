b8138789c75ca28ad28e30bd21ebe51c8216e276
Anthony D'Angelo <ssg2025@hotmail.com>
2012-10-04 18:46:22 -0400

    Created the work branch Question5, created the class Qeustion5.

---
+++ src/edu/carleton/comp4104/assignment1/Question5.java
@@ -0,0 +1,17 @@
+package edu.carleton.comp4104.assignment1;
+
+public class Question5 {
+
+	public Question5() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
