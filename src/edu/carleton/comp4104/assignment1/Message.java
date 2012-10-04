3a135940f9c9a46c0d90a4f8e376d57a0da4bf4e
Anthony D'Angelo <ssg2025@hotmail.com>
2012-10-04 17:03:46 -0400

    Adding the provided Message class.

---
+++ src/edu/carleton/comp4104/assignment1/Message.java
@@ -0,0 +1,39 @@
+/**
+ * Anthony D'Angelo 100773125
+ * Tsering Chopel 100649290
+ * Florent Muyango 100709054 
+ */
+
+package edu.carleton.comp4104.assignment1;
+
+public class Message {
+	int source;
+	int destination;
+	String msg;
+	String time;
+	
+	public int getSource() {
+		return source;
+	}
+	public void setSource(int source) {
+		this.source = source;
+	}
+	public int getDestination() {
+		return destination;
+	}
+	public void setDestination(int destination) {
+		this.destination = destination;
+	}
+	public String getMsg() {
+		return msg;
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public String getTime() {
+		return time;
+	}
+	public void setTime(String time) {
+		this.time = time;
+	}
+}
