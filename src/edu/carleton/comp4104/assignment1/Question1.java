985679e6322db7c7964ed64992828a715ce50aa2
unknown <tchopel@HP3341-0193.windows.scs.carleton.ca>
2012-10-04 18:20:28 -0400

    yesss

---
+++ src/edu/carleton/comp4104/assignment1/Question1.java
@@ -0,0 +1,18 @@
+package edu.carleton.comp4104.assignment1;
+
+public class Question1 implements Runnable{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		//assuming at index 0, we have the first string
+		
+
+	}
+	
+	public void run(){
+		
+	}
+
+}
