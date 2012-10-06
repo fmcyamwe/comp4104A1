dc2007f675d39ace38ccaf4d1a048ed1da9657a8
unknown <tchopel@HP3341-0202.windows.scs.carleton.ca>
2012-10-05 21:54:24 -0400

    only easier part working

--- src/edu/carleton/comp4104/assignment1/Printer.java
+++ src/edu/carleton/comp4104/assignment1/Printer.java
@@ -1,35 +1,58 @@
+/**
+ * Anthony D'Angelo 100773125
+ * Tsering Chopel 100649290
+ * Florent Muyango 100709054 
+ */
+
+
+
 package edu.carleton.comp4104.assignment1;
 
+import java.util.ArrayList;
 import java.util.Random;
+import java.util.concurrent.CountDownLatch;
 
 public class Printer implements Runnable{
-	static boolean [] print;  // need to initialize array by using using size parameter
+	CountDownLatch latch = null;
+	static ArrayList <Boolean> print; 
 	char character;
-	public Printer(char character, int size){
+	int order;
+	
+	
+	public Printer(char character, CountDownLatch latch, int order){
+		this.latch = latch;
 		this.character = character;
-		print[0] = true;
+		this.order = order;
+		//print[0] = true;
 	}
 	
 	public void run(){
+		//System.out.println("am i here"+ Thread.currentThread().getName());
 		Random random = new Random();
 		try {
 			Thread.sleep(random.nextInt(101));
+			//latch.await();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
+
 		
-		while(true){
-			for(int i = 0; i< print.length; i++){
-				if(Thread.currentThread().getName() == "Thread"+i && print[i] == true){   // need to thread.getname right.
-					System.out.print(character);
-					print[i] = false;   // since forloop is inside infinite while loop, cant have same character printing multiple times. so made it false.
-					print[i+1] = true;  // set the next spot  ready for print by making it true.
-				}	
+		synchronized (print){ // am i synchronizing on arraylist object? but its also a variable and that too a static. 
+			while(!(print.get(order) == true)){
+				try {
+					print.wait();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
 			}
+				System.out.println(character);
+				print.set(order, false);
+				print.set(order+1, true);
+				print.notifyAll();
+			
 		}
-		
-		
-		
 	}
+	
+
 
 }
--- src/edu/carleton/comp4104/assignment1/Question1.java
+++ src/edu/carleton/comp4104/assignment1/Question1.java
@@ -6,19 +6,67 @@
 
 package edu.carleton.comp4104.assignment1;
 
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.CountDownLatch;
+
 public class Question1{
 	
 	public static void main(String[] args) {
-		// figure out the string order
-		char [] characters = null; // fill it up with characters in order of first character at zero index
-		int length = args.length;
+		
+		CountDownLatch latch = new CountDownLatch(1);
+		String s = args[0];
+		int length = s.length();
+		char [] characters = new char[length]; // fill it up with characters in order of first character at zero index
+		
 		for(int i = 0;i<length; i++	){
-			Thread t = new Thread(new Printer(characters[i], length), ""+i);
+			characters[i] = s.charAt(i);
+		}
+		
+		Printer.print = new ArrayList<Boolean>(length);
+		for(int i = 0;i<length; i++	){
+			Printer.print.add(i, false); // initializes the new boolean inside Arraylist 
+		}
+		
+		Printer.print.add(0,true);
+		//Printer.print.get(0) = true;
+		
+		for(int i = 0;i<length; i++	){
+			Thread t = new Thread(new Printer(characters[i], latch, i), "Thread "+i);
 			t.start();
 		}
 		
-	
+		try {
+			Thread.sleep(101);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		
+	}
+	
+	public String randomize(String s){
+		if(s.length() == 1)
+			return s;
+		
+		for(char c: s.toCharArray()){
+			if(c=='['){
+				
+			}
+		}
+		return s;
+	}
+	
+	public String onlyOnechar(String s){
+		Random random = new Random();
+		int index = random.nextInt(s.length());
+		char [] c = s.toCharArray();
+		if(c[index] == '[')
+			randomize(s.substring(index));
+		else if(c[index]=='{')
+			onlyOnechar(s.substring(index));
+		else
+			return c[index].;
+		return s;
 	}
 
 }
