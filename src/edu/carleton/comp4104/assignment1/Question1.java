313f2fd9c2372705ba719b3cc56047a2108f5e6a
chopel tsering <chopeltsering@gmail.com>
2012-10-06 04:11:43 -0400

    trying to get the curly braced right first

--- src/edu/carleton/comp4104/assignment1/Question1.java
+++ src/edu/carleton/comp4104/assignment1/Question1.java
@@ -15,36 +15,50 @@ public class Question1{
 	public static void main(String[] args) {
 		
 		//CountDownLatch latch = new CountDownLatch(1);
-		String s = args[0];
-		int length = s.length();
-		char [] characters = new char[length]; // fill it up with characters in order of first character at zero index
+		String st = args[0];
+		//String copy = st;
+		int length = st.length();
 		
-		for(int i = 0;i<length; i++	){
-			characters[i] = s.charAt(i);
+		
+		//System.out.println(ch);
+		
+		for(int i = 0; i< st.length(); i++){
+			if(st.charAt(i)=='{'){  // need to take care of embedded curly brackets. {{}}, right now first and third curly bracket are considered one
+				//System.out.println("{ was detected, so i can use == to compare");
+				for(int j = 0; j< st.substring(i).length(); j++){
+					if(st.substring(i).charAt(j)=='}'){
+						char c = onlyOnechar(st.substring(i, j));
+						System.out.println(c);
+						i += j+1;
+					}
+				}	
+			}
+			System.out.println(st.charAt(i));
 		}
 		
-		Printer.print = new ArrayList<Boolean>(length);
-		for(int i = 0;i<length; i++	){
-			Printer.print.add(i, false); // initializes the new boolean inside Arraylist 
-		}
+//		char [] characters = new char[length]; // fill it up with characters in order of first character at zero index
+//		
+//		for(int i = 0;i<length; i++	){
+//			characters[i] = s.charAt(i);
+//		}
+//		
+//		Printer.print = new ArrayList<Boolean>(length);
+//		for(int i = 0;i<length; i++	){
+//			Printer.print.add(i, false); // initializes the new boolean inside Arraylist 
+//		}
+//		
+//		Printer.print.add(0,true);
+//		
+//		for(int i = 0;i<length; i++	){
+//			Thread t = new Thread(new Printer(characters[i], i), "Thread "+i);
+//			t.start();
+//		}
 		
-		Printer.print.add(0,true);
-		//Printer.print.get(0) = true;
 		
-		for(int i = 0;i<length; i++	){
-			Thread t = new Thread(new Printer(characters[i], i), "Thread "+i);
-			t.start();
-		}
-		
-		try {
-			Thread.sleep(101);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
 		
 	}
 	
-	public String randomize(String s){
+	public static String randomize(String s){
 		if(s.length() == 1)
 			return s;
 		
@@ -56,17 +70,18 @@ public class Question1{
 		return s;
 	}
 	
-	public String onlyOnechar(String s){
+	public static char onlyOnechar(String s){
 		Random random = new Random();
 		int index = random.nextInt(s.length());
 		char [] c = s.toCharArray();
+		
 		if(c[index] == '[')
 			randomize(s.substring(index));
 		else if(c[index]=='{')
-			onlyOnechar(s.substring(index));
+			 return onlyOnechar(s.substring(index+1));
 		else
-			return c[index].;
-		return s;
+			return c[index];
+		return 0;
 	}
 
 }
