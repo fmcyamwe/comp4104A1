5b97454fe8d1710e4aa9c9a7b16b6b281a237709
Anthony D'Angelo <ssg2025@hotmail.com>
2012-10-08 22:10:02 -0400

    Added a test file for Q4, wrote some parsing code that turns it into an arraylist of arraylists of chars.

--- src/edu/carleton/comp4104/assignment1/Question4.java
+++ src/edu/carleton/comp4104/assignment1/Question4.java
@@ -2,12 +2,11 @@ package edu.carleton.comp4104.assignment1;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
-
-
 
 public class Question4 {
 
@@ -15,63 +14,91 @@ public class Question4 {
 	 * @param args
 	 */
 	static String order;
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		File inputFile= null;
-		ArrayList<String>  products=new ArrayList<String>(); 
-		
-		for(int i =0;i<=4; i=i+2){
-			
-			switch(args[i]){
+		String inputFile = null;
+		ArrayList<String> products = new ArrayList<String>();
+
+		for (int i = 0; i <= 4; i = i + 2) {
+
+			switch (args[i]) {
 			case "-c":
-				inputFile= new File("C:\\Users\\Temp\\workspace\\q2\\src\\edu\\carleton\\comp4104\\assignment1\\" + args[i+1]);
+				inputFile = new java.io.File("").getAbsolutePath()
+						+ File.separator + args[i + 1];
 				break;
-			case"-b":
-				for(int e = i+1 ; e < args.length; e++ ){
+			case "-b":
+				for (int e = i + 1; e < args.length; e++) {
 					products.add(args[e]);
 				}
 				break;
 			}
-			
+
 		}
-		
-		getContents(inputFile);
+
+		FileReader r = null;
+		try {
+			r = new FileReader(inputFile);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		getContents(r);
 
 	}
 
-	public static void getContents(File inputFile) {
-		
-		  String line = null; 
+	public static ArrayList<ArrayList<Character>> getContents(FileReader inputFile) {
+
+		String line = null;
 		String REGEX = "\\s*(\\s|,)\\s*";
-	    Pattern p = Pattern.compile(REGEX);
-	    
-	    try {
-		      //use buffering, reading one line at a time
-		      
-		      BufferedReader input =  new BufferedReader(new FileReader(inputFile));
-		      try{ 
-		    	  
-		        while (( line = input.readLine()) != null){
-	        		
-		        		String[] items = p.split(line);
-		        		
-		        		for(int i =0 ; i< items.length;i++){
-		        			System.out.println(items[i]);
-		        		}
-		        
-		        }
-		      }
-		      finally {
-		    	  
-		        input.close();
-		      }
-		    }
-		    catch (IOException ex){
-		      ex.printStackTrace();
-		    }
-		    
+		Pattern p = Pattern.compile(REGEX);
+		ArrayList<ArrayList<Character>> myReturn = new ArrayList<ArrayList<Character>>();
 		
+		try {
+			// use buffering, reading one line at a time
+
+			BufferedReader input = new BufferedReader(inputFile);
+
+			try {
+
+				while ((line = input.readLine()) != null) {
+
+					String[] items = p.split(line);
+					switch(items[1]){
+						case "has-parts":
+							myReturn.add(new ArrayList<Character>());
+							ArrayList<Character> temp = myReturn.get(myReturn.size() - 1);
+							temp.add(items[0].charAt(0));
+							for(int i = 2; i < items.length; ++i){
+								temp.add(items[i].charAt(0));
+							}
+							break;
+							
+						case "before":
+							myReturn.add(new ArrayList<Character>());
+							ArrayList<Character> tempBefore = myReturn.get(myReturn.size() - 1);
+							//Swap them so that the X before Y becomes Y has-parts X. Not that it has those parts, but it must be built in that order.
+							//Assume there is only one letter after 'before'.
+							tempBefore.add(items[2].charAt(0));
+							tempBefore.add(items[0].charAt(0));
+							break;	
+							
+						default:
+							System.out.println("Error: the second token on every line of the input file should be either 'has-parts' or 'before'.");
+							break;
+					}
+
+				}
+			} finally {
+
+				input.close();
+			}
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+		
+		return myReturn;
 	}
 
 }
