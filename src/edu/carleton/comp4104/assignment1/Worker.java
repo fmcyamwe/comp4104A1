08835bd4fa4ce9ab64a7ca059d2712458e2ded59
unknown <tchopel@HP4115-0378.windows.scs.carleton.ca>
2012-10-08 22:43:21 -0400

    Question 4

--- src/edu/carleton/comp4104/assignment1/Worker.java
+++ src/edu/carleton/comp4104/assignment1/Worker.java
@@ -1,29 +1,53 @@
 package edu.carleton.comp4104.assignment1;
 
+import java.util.ArrayList;
+import java.util.Stack;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 public class Worker implements Runnable {
-	int numOfWorkers;
 	CyclicBarrier barrier;
-	String character;
+	char character;
+	ArrayList<ArrayList<Character>> dependents = new ArrayList<ArrayList<Character>>();
+	Stack <String> stack = new Stack<String> ();
+	boolean [] check = new boolean[dependents.size()];
 	
-	public Worker(int i,String c, CyclicBarrier b){
-		this.numOfWorkers=i;
+	public Worker(char c, CyclicBarrier b, ArrayList<ArrayList<Character>> dependents ){
+		System.out.println(c);
 		this.barrier=b;
 		this.character=c;
+		this.dependents = dependents;
 	}
 	
-	
-	@Override
+
 	public void run() {
-		// TODO Auto-generated method stub
-		if(numOfWorkers<2){
-			System.out.println("Build"+ character);
-			
-		}else{
-			CyclicBarrier b = new CyclicBarrier(numOfWorkers-1);
-			//new Thread(new Worker()).start();
+		for(int j = 0; j < check.length; j++){
+			if(check[j] == false){
+				for(int index = 0; index < dependents.size(); index++){
+					for(char c: dependents.get(index)){
+						if(c == character){
+							for(int i = 0; i< dependents.get(index).size(); i++){
+								System.out.println("i am here");
+								Thread thread = new Thread(new Worker(dependents.get(index).get(i), new CyclicBarrier(dependents.get(i+1).size()), dependents));
+								thread.start();
+							}
+						}
+					}
+				check[index] = true;
+				}
+			}
 		}
+		
+		stack.push("build "+ character);
+		try {
+			barrier.await();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (BrokenBarrierException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(stack.pop());
 		
 	}
 
